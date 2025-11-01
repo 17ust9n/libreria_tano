3dc8b62ca62c0072be0133e16298b520d6d8c88d
17ust9n <paolaverre@gmail.com>
2025-11-01 00:05:14 -0300

    Ya funciona

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/anadir">Añadir Libro</Link></li>
-      </ul>
+    <nav className="navbar">
+      <h1 className="navbar-title">Mi Librería</h1>
+      
+      <div className="menu-container">
+        <button className="menu-button" onClick={toggleMenu}>
+          Menú {/* Texto del botón */}
+        </button>
+        
+        {menuOpen && (
+          <div className="menu-dropdown">
+            <Link to="/" onClick={toggleMenu}>Inicio</Link>
+            <Link to="/anadir" onClick={toggleMenu}>Añadir Libro</Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
